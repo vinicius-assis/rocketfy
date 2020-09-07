e9f68b16d8298c672c9e160e2ff6be061452a515
Vinicius <viniciusassis.dev@hotmail.com>
2020-09-07 14:52:20 -0300

    Create Card component

---
+++ src/components/Card/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { Container, Label } from './styles'
+
+const Card = () => {
+  return (
+    <Container>
+      <header>
+        <Label color="#7159c1" />
+      </header>
+      <p>Fazer a migração completa de servidor!</p>
+      <img src="#" alt="#"/>
+    </Container>    
+  )
+}
+
+export default Card
