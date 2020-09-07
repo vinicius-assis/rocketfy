8f4132fdf1d7ab96fe058d7eecdd7042b01c7fb4
Vinicius <viniciusassis.dev@hotmail.com>
2020-09-07 14:52:03 -0300

    Create Board component

---
+++ src/components/Board/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import List from '../List/index'
+
+import { Container } from './styles'
+
+const Board = () => {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
+
+export default Board
