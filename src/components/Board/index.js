503b6784c2ed8a38b08d89ccbe455534264b444f
Vinicius <viniciusassis.dev@hotmail.com>
2020-09-07 15:26:49 -0300

    Use api to render cards

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import { loadLists } from '../../service/api'
 
 import List from '../List/index'
 
 import { Container } from './styles'
 
+const lists = loadLists()
+
 const Board = () => {
   return (
     <Container>
-      <List />
+      {lists.map(list => <List key={list.title} data={list} />)}
     </Container>
   )
 }
