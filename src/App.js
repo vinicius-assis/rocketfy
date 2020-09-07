187d600c5771c6700e96e2b187e516a21cd6bdf2
Vinicius <viniciusassis.dev@hotmail.com>
2020-09-07 16:23:57 -0300

    Add dnd-react in card component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+
+
 import GlobalStyle from './styles/global'
 import Header from './components/Header';
 import Board from './components/Board/index'
 
 function App() {
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
       <GlobalStyle />
       <Header />
       <Board />
-    </>
+    </DndProvider>
   );
 }
 
